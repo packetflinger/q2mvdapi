01250ef43ce72c873558c648e26a7ecf6c785cb2
Joe Reid <joe@joereid.com>
2022-09-08 22:19:03 -0400

    ACL working

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
 	"strings"
 )
@@ -13,6 +14,8 @@ import (
 var Port *int
 var Listen string
 var Target *string
+var ACLDomain *string
+var AllowedIPs []string
 
 type Files struct {
 	Filename []string
@@ -22,7 +25,18 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "")
 }
 
+// enumerate all the files in the specific directory.
+// If we find MVD files, add to the structure then
+// marshal the structure into JSON and send it
 func ListFiles(w http.ResponseWriter, r *http.Request) {
+	// have to split, looks like [::1]:xxxxx otherwise
+	ip, _, _ := net.SplitHostPort(r.RemoteAddr)
+	if !CheckACL(ip, AllowedIPs) {
+		fmt.Fprintln(w, "Not allowed")
+		log.Printf("Auth: %s not allowed\n", ip)
+		return
+	}
+
 	filesfound := Files{}
 	files, err := ioutil.ReadDir(*Target)
 	if err != nil {
@@ -48,14 +62,39 @@ func handleRequests() {
 	log.Fatal(http.ListenAndServe(Listen, nil))
 }
 
+// Request is allowed or not
+func CheckACL(needle string, haystack []string) bool {
+	for _, ip := range haystack {
+		if needle == ip {
+			return true
+		}
+	}
+
+	return false
+}
+
 func main() {
 	handleRequests()
 }
 
+// Parse the flags and setup the IPs allowed
 func init() {
 	Port = flag.Int("p", 27999, "The TCP port to listen on")
 	Target = flag.String("d", ".", "The directory to look in")
+	ACLDomain = flag.String("acl", "_acl.pfl.gr", "DNS name of allowed IPs")
 	flag.Parse()
 
 	Listen = fmt.Sprintf(":%d", *Port)
+
+	// We use a simple DNS name for an ACL.
+	// Look up the name, any IP address that resolves (v4 and v6) will be allowed
+	ips, err := net.LookupIP(*ACLDomain)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, ip := range ips {
+		AllowedIPs = append(AllowedIPs, ip.String())
+		log.Println("Allowed IP: " + ip.String())
+	}
 }
